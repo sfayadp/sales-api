9c0a892b76ab1b883fc5bace0e2726472d5c7b33
sfayadp <stefan0710f@gmail.com>
2022-08-15 13:06:02 -0500

    Enable cors

--- Sales.Api/Program.cs
+++ Sales.Api/Program.cs
@@ -8,6 +8,18 @@ using Sales.Api.Services.Products;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:4200",
+                                              "https://localhost:4200/");
+                      });
+});
+
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -32,8 +44,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(MyAllowSpecificOrigins);
+
 app.UseAuthorization();
 
 app.MapControllers();
+
 
 app.Run();
